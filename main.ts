fccdbdb1061116d611815c8666b026e59a1f0087
yaochengkuo <115142539+yaochengkuo@users.noreply.github.com>
2022-12-08 16:39:03 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,31 @@
-basic.forever(function () {
-	
+let x = 0
+let y = 0
+let list: number[] = []
+let n = 0
+let a = 0
+function 燈數 (num: number) {
+    if (num % 5 == 0) {
+        x = 4
+        y = Math.floor(num / 5) - 1
+    } else {
+        x = num % 5 - 1
+        y = Math.floor(num / 5)
+    }
+    led.plot(x, y)
+}
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    list = []
+    for (let index = 0; index < 25; index++) {
+        list.push(0)
+    }
+    n = 0
+    while (n < 3) {
+        a = randint(0, 24)
+        if (list[a] == 0) {
+            list[a] = 1
+            n += 1
+            燈數(a + 1)
+        }
+    }
 })
