43f359ec1777bdd58596e1038afbd1e6a0d0d2b8
yaochengkuo <115142539+yaochengkuo@users.noreply.github.com>
2022-12-08 16:45:59 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,8 @@
-let x = 0
-let y = 0
 let list: number[] = []
 let n = 0
 let a = 0
-function 燈數 (num: number) {
-    if (num % 5 == 0) {
-        x = 4
-        y = Math.floor(num / 5) - 1
-    } else {
-        x = num % 5 - 1
-        y = Math.floor(num / 5)
-    }
-    led.plot(x, y)
-}
+let x = 0
+let y = 0
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
     list = []
@@ -25,7 +15,17 @@ input.onButtonPressed(Button.A, function () {
         if (list[a] == 0) {
             list[a] = 1
             n += 1
-            燈數(a + 1)
+            燈(a + 1)
         }
     }
 })
+function 燈 (num: number) {
+    if (num % 5 == 0) {
+        x = 4
+        y = Math.floor(num / 5) - 1
+    } else {
+        x = num % 5 - 1
+        y = Math.floor(num / 5)
+    }
+    led.plot(x, y)
+}
